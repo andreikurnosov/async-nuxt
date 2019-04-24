1dbda0f844db344156e54504c598335809259810
nixdie <nixdie@yandex.ru>
2019-04-24 21:22:16 +0700

    fetch data with axios

--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -8,6 +8,7 @@
 </template>
 
 <script>
+import axios from 'axios'
   export default {
     head () {
       return {
@@ -27,11 +28,9 @@
       }
     },
     mounted () {
-      fetch(`https://jsonplaceholder.typicode.com/posts/${this.id}`)
+      axios.get(`https://jsonplaceholder.typicode.com/posts/${this.id}`)
         .then(res => {
-          res.json().then(post => {
-            this.post = post
-          })
+          this.post = res.data
         })
     }
   }
